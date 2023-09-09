81d30575ea1956cbfb66078791c31740f4c59625
mhadziqrazin <mhadziq0403@gmail.com>
2023-09-09 19:08:19 +0700

    feat: test alur module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('hello')
+  async getHello() {
+    return this.authService.getHello();
+  }
+
+  @Post('create')
+  async create(
+    @Body('nama') nama: string,
+    @Body('alamat') alamat: string,
+    @Body('kapasitas') kapasitas: number,
+  ) {
+    const stadium = await this.authService.create(nama, alamat, kapasitas);
+    return stadium;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  getHello() {
+    return this.prisma.stadium.findMany();
+  }
+
+  async create(nama: string, alamat: string, kapasitas: number) {
+    const stadium = await this.prisma.stadium.create({
+      data: {
+        nama,
+        alamat,
+        kapasitas,
+      },
+    });
+    return stadium;
+  }
+}
